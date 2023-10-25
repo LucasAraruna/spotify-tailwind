f647f93f189e34b26df49a402a44cb5bf9ad82df
Lucas <lucas.araruna07@gmail.com>
2023-10-25 13:50:51 -0300

    creating grid below albums

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from 'lucide-react'
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -57,36 +57,61 @@ export default function Home() {
           <h1 className='font-semibold text-3xl mt-10'> Good Afternoon</h1>
 
           <div className='grid grid-cols-3 gap-4 mt-4'>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/fearofthedark.jpg" width={104} height={104} alt='Capa do Album Fear Of The Dark da Banda Iron Maiden' />
 
               <strong>Fear Of The Dark</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/meteora.jpg" width={104} height={104} alt='Capa do Album Meteora da banda Linkin Park' />
               <strong>Meteora</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/hybridtheory.jpg" width={104} height={104} alt='Capa do Album Hybrid Theory da Banda Linkin Park' />
 
               <strong>Hybrid Theory</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/masterofpuppets.jpg" width={104} height={104} alt='Capa do Album Master Of Puppets da Banda Metallica' />
               <strong>Master Of Puppets</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/waristheanswer.jpg" width={104} height={104} alt='Capa do Album War is The Answer da Banda Five Finger Death Punch' />
               <strong>War is The Anwser</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
-            <a href='#' className='bg-white/10 rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
+            <a href='#' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
               <Image src="/askingalexandria.jpg" width={104} height={104} alt='Capa do Album Asking Alexandria da Banda Asking Alexandria' />
 
               <strong>Asking Alexandria</strong>
+
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible'>
+                <Play />
+              </button>
             </a>
 
           </div>
 
+          <h2 className='font-semibold text-2xl mt-10'>Made for Lukirito</h2>
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
